54b9b20167359688a8c5d7ebd68ca90ea2ebadea
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-01-21 17:31:05 -0500

    Update React app

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,16 +1,49 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+
+const [data, setData]= useState ();
+const [loading, setLoading] = useState(true);
+const [error, setError]= useState ();
+
+useEffect (()=>{
+  axios ('http://20.151.204.61:8080/connections/')
+  .then(res => {
+    setData(res.data)
+    console.log(data)
+  })
+  .catch (err =>{
+    console.log('error fetching data', err);
+    setError(error);
+   
+  })
+  .finally (()=>{
+    setLoading(false);
+  })
+},[]);
+
+  if (loading) return 'loading...';
+
+
+
+
   return (
     <div className="App">
       <h3> Check Status</h3>
       <br></br>
-      <button type="button">Status</button>
-      <p> isloading </p>
+      <button onClick ={()=>setData(data.rfc23_state)} type="button">Status</button>
+      <p> isloading  </p>
       <h3> Send Response </h3>
       <br></br>
-      <button> Accept</button><button> Decline</button>
-      <p> isLoading </p>
+      <button onClick={()=>setData(data.rfc23_state)}  type="button"> Accept</button>
+      <p> isLoading  </p>
+      <br></br>
+      <button onClick={()=>setData(data.rfc23_state)} type="button"> Decline</button>
+      <p> isLoading  </p>
     </div>
   );
 }
