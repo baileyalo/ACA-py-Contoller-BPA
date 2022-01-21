f1a2718565e8c95e56bcd007017d998fbbc75989
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-01-20 23:34:59 -0500

    React App

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Check Status</h3>
+      <br></br>
+      <button type="button">Status</button> <p> isloading </p>
+      <h3> Send Response </h3>
+      <button> Accept</button><button> Decline</button>
+      <p> isLoading </p>
+ 
+
+    </div>
+  );
+}
+
+export default App;
