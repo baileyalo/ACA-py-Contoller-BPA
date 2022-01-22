f69d6a5145a31fd0cf900d32e7a1435c286d2518
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-01-22 00:37:05 -0500

    update server app.js

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,12 @@
 const express    = require('express');        
 const app        = express();               
 const bodyParser = require('body-parser');
-const { response } = require('express');
+const axios = require ('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3001;        
+const port = process.env.PORT || 3000;        
 const router = express.Router();  
 
 
@@ -15,6 +15,9 @@ const router = express.Router();
 
 //routes created 
 app.use(express.static(__dirname+'/topic'));
+
+app.use('/topic', router);
+
 router.get('/', function(req, res) {
     res.send('Welcome to our Topics!' );
 });
@@ -23,7 +26,7 @@ router.post('/connections', function(req, res) {
     console.log(req.body);
     currstate= req.body?.rfc23_state;
     
-    switch (state){
+    switch (currstate){
         case "invitation-sent":
             console.log ('Invitation created ');
             console.log ('connection ID:', req.body?.connection_id);
@@ -31,38 +34,40 @@ router.post('/connections', function(req, res) {
         case 'request-recieved':
             console.log ('connection ID:', req.body?.connection_id);
             console.log ('Request Accepted');
-            break;
-        case "response-sent":
-          console.log("*** Invitation Response sent to invitee");
-          console.log("Connection ID=", req.body?.connection_id);
-          break;
-        case "completed":
-          console.log("*** Connection is complete!");
-          console.log("Connection ID=", req.body?.connection_id);
-          break;
-        case "undefined":
-          console.log("XXX Unkown connection state");
-          console.log("Connection ID=", req.body?.connection_id);
+            
+            restend = 'http://20.151.204.61:8080/connections/'; 
 
-            restapi= 'http://20.151.204.61:8080/connections/' + req.body?.connection_id + 'accept-request?my_endpoint=http%3A%2F%2F20.151.204.61%3A8000';
-        res.send(currstate);   
-        };    
-
-        axios
-        .post(restapi)
-        .then(response =>{
-        console.log ('statuscode: ${response.status}');
-
+          restapi= restend + req.body?.connection_id + 'accept-request?my_endpoint='+ encodeURI(restend);
+          restData = {};
+      restHeaders = {
+        headers: {
+        }
+      };
+      axios
+        .post(restURL, restData, restHeaders)
+        .then(res => {
+          console.log('statusCode: ${res.status}');
         })
-        .catch(err => {
-          console.error(err);
-        })      
-    
-    res.status(200).send("OK");
+        .catch(error => {
+          console.error(error);
+        })
 
-    });
-    
+      break;
+    case "response-sent":
+      console.log("*** Invitation Response sent to invitee");
+      console.log("Connection ID=", req.body?.connection_id);
+      break;
+    case "completed":
+      console.log("*** Connection is complete!");
+      console.log("Connection ID=", req.body?.connection_id);
+      break;
+    case "undefined":
+      console.log("XXX Unkown connection state");
+      console.log("Connection ID=", req.body?.connection_id);
 
-app.use('/topic', router);
+  }
+  res.status(200).send("OK");
+});
+
 app.listen(port);
 console.log('listening on port ' + port);
